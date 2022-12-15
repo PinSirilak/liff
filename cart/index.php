767ccd61281cbbc8348338ef74e99ce971f91019
Sirilak Rakamduang <sirilak.r@themobic.com>
2022-12-15 17:54:14 +0700

    update_20221215

---
+++ cart/index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Rintip</title>
+
+
+
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@400&display=swap" rel="stylesheet">
+
+
+
+
+  <link rel="stylesheet" href="../css/style.css" media="all">
+  <link rel="stylesheet" href="../css/bootstrap.min.css" media="all" />
+  <link rel="stylesheet" href="../css/animate.css" media="all">
+  <link rel="stylesheet" href="../css/font.awesome.css" media="all">
+  <link rel="stylesheet" href="../css/ionicons.min.css" media="all">
+  <link rel="stylesheet" href="../css/jquery-ui.min.css" media="all">
+  <link rel="stylesheet" href="../css/slick.css" media="all">
+  <link rel="stylesheet" href="../css/magnific-popup.css" media="all">
+  <link rel="stylesheet" href="../css/owl.carousel.min.css" media="all">
+  <link rel="stylesheet" href="../css/linearicons.css" media="all">
+  <link rel="stylesheet" href="../css/slinky.menu.css" media="all">
+  <link rel="stylesheet" href="../css/plugins.css" media="all">
+
+
+
+
+  <script src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/isotope.pkgd.min.js"></script>
+  <script src="../js/jquery-waypoints.js"></script>
+  <script src="../js/jquery.countdown.js"></script>
+  <script src="../js/jquery.counterup.min.js"></script>
+  <script src="../js/jquery.elevatezoom.js"></script>
+  <script src="../js/jquery.instagramFeed.min.js"></script>
+  <script src="../js/jquery.magnific-popup.min.js"></script>
+  <script src="../js/jquery.ui.js"></script>
+  <script src="../js/main.js"></script>
+  <script src="../js/map.js"></script>
+  <script src="../js/owl.carousel.min.js"></script>
+  <script src="../js/plugins.js"></script>
+  <script src="../js/popper.js"></script>
+  <script src="../js/slick.min.js"></script>
+  <script src="../js/slinky.menu.js"></script>
+  <script src="../js/tippy-bundle.umd.js"></script>
+  <script src="../js/vconsole.min.js"></script>
+
+
+
+  <style>
+  .sizeImgLogo {
+    width: 60px;
+  }
+
+  .sizeImgMenu {
+    width: 21px;
+    margin-top: 12px;
+  }
+
+  .broderBotton {
+    border-bottom: 1px solid #d5d5d5;
+
+  }
+
+  .pb-0 {
+    padding-bottom: 0px
+  }
+
+
+  .cardBox {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #fff;
+    background-clip: border-box;
+    /* border: 1px solid rgba(0, 0, 0, .125); */
+    border-radius: .25rem;
+    margin: 10px;
+    padding: 10px
+  }
+
+  .imgPro3 {
+    width: 100px;
+    margin: 15px;
+  }
+
+  .p {
+    font-size: 14px;
+    margin-top: 15px;
+    font-weight: bold;
+    margin-left: 30px;
+  }
+
+  .pTotal {
+    font-size: 16px;
+    margin-top: 15px;
+    font-weight: bold;
+    margin-left: 30px;
+    color: #0bbc0d;
+  }
+
+  .pTotal_1 {
+    font-size: 16px;
+    margin-top: 15px;
+    font-weight: bold;
+    margin-left: 10px;
+  }
+
+  p {
+    border-bottom: 0px dashed #ddd;
+  }
+
+  .pDetail {
+    font-size: 12px;
+    margin-top: -15px;
+    margin-left: 30px;
+  }
+
+
+  .wInput {
+    width: 45%
+  }
+
+  .mt-30 {
+    margin: 50% 0 10%;
+  }
+
+
+  .rLayOut {
+    font-size: 14px;
+    margin-top: 5px;
+    font-weight: bold;
+    margin-left: 30px;
+
+  }
+
+  .broderB {
+    border-bottom: 1px solid #d5d5d5;
+    margin: 10px;
+  }
+
+  .abtn {
+    background-color: #0bbc0d;
+    border-color: #0bbc0d;
+  }
+
+
+  .wText {
+    font-size: 18px;
+    font-weight: bold;
+    color: #ffff;
+    margin-bottom: 0px;
+  }
+  </style>
+
+</head>
+
+<body style="background-color: #ffff;">
+
+
+  <div class="d-flex broderBotton">
+    <div class="me-auto p-2 pb-0">
+      <img class="sizeImgLogo" src="../image/100.png" alt="" />
+    </div>
+
+    <div class="me-auto p-3 pb-0 ">
+      <p class="front"><b>ตะกร้าสินค้า</b></p>
+    </div>
+    <div class="p-2 pb-0">
+      <img class="sizeImgMenu" src="../image/Component_161.png" alt="" />
+    </div>
+  </div>
+
+
+  <div class="broderB">
+    <div class="row">
+      <div class="col-3">
+        <img class="imgPro3" src="../image/P3.jpg" alt="...">
+      </div>
+      <div class="col-9">
+        <p class="p">น้ำมันปาล์มโอเลอีนแบบขวด ขนาด 1 ลิตร</p>
+        <p class="pDetail">บรรจุ 12 ขวด / กล่อง</p>
+
+        <div class="row">
+          <div class="col-7">
+            <div class="product_variant quantity rLayOut">
+              <label>จำนวน &nbsp;&nbsp;</label><input class="wInput" min="1" value="1" type="number">
+            </div>
+          </div>
+          <div class="col-2 rLayOut">
+            590
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="broderB">
+    <div class="row">
+      <div class="col-3">
+        <img class="imgPro3" src="../image/P4.jpg" alt="...">
+      </div>
+      <div class="col-9">
+        <p class="p">น้ำมันปาล์มโอเลอีนแบบถุงขนาด 1 ลิตร</p>
+        <p class="pDetail">บรรจุ 12 ถุง / กล่อง</p>
+
+        <div class="row">
+          <div class="col-7">
+            <div class="product_variant quantity rLayOut">
+              <label>จำนวน &nbsp;&nbsp;</label><input class="wInput" min="1" value="1" type="number">
+            </div>
+          </div>
+          <div class="col-2 rLayOut">
+            590
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="broderB">
+    <div class="row">
+      <div class="col-9">
+        <p class="pTotal_1">รวม</p>
+      </div>
+      <div class="col-3">
+        <p class="pTotal">4,130</p>
+      </div>
+    </div>
+  </div>
+
+
+
+  <!-- <div class="d-inline-flex p-2"> -->
+  <div class="d-grid gap-5 col-10 mx-auto">
+    <button class="btn abtn mt-30" type="button">
+      <p class="wText">ดำเนินการต่อ</p>
+    </button>
+  </div>
+
+  <!-- </div> -->
+
+
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Rintip</title>
+
+
+
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@400&display=swap" rel="stylesheet">
+
+
+
+
+  <link rel="stylesheet" href="css/style.css" media="all">
+  <link rel="stylesheet" href="css/bootstrap.min.css" media="all" />
+  <link rel="stylesheet" href="css/animate.css" media="all">
+  <link rel="stylesheet" href="css/font.awesome.css" media="all">
+  <link rel="stylesheet" href="css/ionicons.min.css" media="all">
+  <link rel="stylesheet" href="css/jquery-ui.min.css" media="all">
+  <link rel="stylesheet" href="css/slick.css" media="all">
+  <link rel="stylesheet" href="css/magnific-popup.css" media="all">
+  <link rel="stylesheet" href="css/owl.carousel.min.css" media="all">
+  <link rel="stylesheet" href="css/linearicons.css" media="all">
+  <link rel="stylesheet" href="css/slinky.menu.css" media="all">
+  <link rel="stylesheet" href="css/plugins.css" media="all">
+
+
+
+
+  <script src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/isotope.pkgd.min.js"></script>
+  <script src="js/jquery-waypoints.js"></script>
+  <script src="js/jquery.countdown.js"></script>
+  <script src="js/jquery.counterup.min.js"></script>
+  <script src="js/jquery.elevatezoom.js"></script>
+  <script src="js/jquery.instagramFeed.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/jquery.ui.js"></script>
+  <script src="js/main.js"></script>
+  <script src="js/map.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/slick.min.js"></script>
+  <script src="js/slinky.menu.js"></script>
+  <script src="js/tippy-bundle.umd.js"></script>
+  <script src="js/vconsole.min.js"></script>
+
+  <style>
+  .sizeImgLogo {
+    width: 60px;
+  }
+
+  .front {
+    size: 12px;
+  }
+
+
+  .sizeImgProdeuct {
+    width: 100%;
+    margin-top: -20px
+  }
+
+  .sizeImgMenu {
+    width: 21px;
+    margin-top: 12px;
+  }
+
+  .sizeImgฺBasket {
+    width: 20px;
+    margin-top: 10px;
+  }
+
+  .frontPro {
+    font-size: 16px;
+    font-weight: bold;
+    margin-top: 10px
+  }
+
+  .frontProDetail {
+    font-size: 12px;
+    margin-top: -10px
+  }
+
+  .wInput {
+    width: 50%
+  }
+
+  .rLayOut {
+    margin: 10px
+  }
+
+
+  .ImgฺBasketPro {
+    width: 15px;
+  }
+
+  .card {
+    margin-bottom: 10px;
+    background-color: #ffff;
+  }
+
+  p {
+    border-bottom: 0px dashed #ddd;
+  }
+
+
+
+  .abtn {
+    background-color: #0bbc0d;
+    border-color: #0bbc0d;
+    color: #ffff;
+  }
+  </style>
+</head>
+
+<body style="background-color: #d9d9d9;">
+
+
+
+  <div class="d-flex mb-3 " style="background-color: #ffff;">
+    <div class="me-auto p-2">
+      <img class="sizeImgLogo" src="image/100.png" alt="" />
+    </div>
+
+    <div class="me-auto p-3">
+      <p class="front"><b>รายการสินค้า</b></p>
+    </div>
+    <div class="p-2">
+      <!-- <img class="sizeImgฺBasket" src="image/Group_2.png" alt="" /> -->
+      <a href="cart/index.php"><img class="sizeImgฺBasket" src="image/Group_2.png" alt="" /></a>
+    </div>
+    <div class="p-2">
+      <img class="sizeImgMenu" src="image/Component_161.png" alt="" />
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+  <div class="card">
+    <div class="d-flex">
+      <img class="sizeImgProdeuct" src="image/P3.jpg" alt="" />
+    </div>
+
+    <div class="d-flex">
+      <div class="p-2 w-100 frontPro">น้ำมันปาล์มโอเลอีนแบบขวด ขนาด 1 ลิตร</div>
+      <div class="p-2 flex-shrink-1 frontPro">590</div>
+    </div>
+
+    <div class="d-flex">
+      <div class="p-2 w-100 frontProDetail">บรรจุ 12 ขวด / กล่อง</div>
+    </div>
+
+
+    <div class="d-flex row rLayOut">
+      <div class="p-2  col-7">
+        <div class="product_variant quantity">
+          <label>จำนวน &nbsp;&nbsp;</label><input class="wInput" min="1" value="1" type="number">
+          &nbsp;&nbsp;<label>ลัง</label>
+        </div>
+      </div>
+      <div class="p-2  col-5">
+        <button type="button" class="btn abtn "><img class="ImgฺBasketPro" src="image/Group_1.png" alt="">
+          &nbsp;ใส่ตะกร้า</button>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="card">
+    <div class="d-flex">
+      <img class="sizeImgProdeuct" style="margin-top: 0px" src="image/P4.jpg" alt="" />
+    </div>
+
+    <div class="d-flex">
+      <div class="p-2 w-100 frontPro">นน้ำมันปาล์มโอเลอีนแบบถุงขนาด 1 ลิตร</div>
+      <div class="p-2 flex-shrink-1 frontPro">590</div>
+    </div>
+
+
+
+    <div class="d-flex">
+      <div class="p-2 w-100 frontProDetail">บรรจุ 12 ถุง / กล่อง</div>
+    </div>
+
+
+
+    <div class="d-flex row rLayOut">
+      <div class="p-2  col-7">
+        <div class="product_variant quantity">
+          <label>จำนวน &nbsp;&nbsp;</label><input class="wInput" min="1" value="1" type="number">
+          &nbsp;&nbsp;<label>ลัง</label>
+        </div>
+      </div>
+      <div class="p-2  col-5">
+        <button type="button" class="btn abtn "><img class="ImgฺBasketPro" src="image/Group_1.png" alt="">
+          &nbsp;ใส่ตะกร้า</button>
+      </div>
+    </div>
+  </div>
+
+
+
+</body>
+
+</html>
